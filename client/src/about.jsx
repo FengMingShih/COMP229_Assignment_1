c309ed8bca2c63e6aff84812de10b6cdc5b08493
Feng Ming Shih <fengmingshih@gmail.com>
2024-05-26 23:12:13 -0400

    Second commit

--- client/src/about.jsx
+++ client/src/about.jsx
@@ -6,7 +6,7 @@ Date: 2024-05-25
 */
 import me from './assets/me.jpg';
 import './about.css';
-
+import resume from './assets/resume.pdf';
 export default function About() {
     return (
         <div className="about">
@@ -14,7 +14,7 @@ export default function About() {
             <img src={me} alt="Feng Ming Shih" className='me-image' />
             <p>My name is Feng Ming Shih, and I am a software developer with 7 years of experience in web development and system maintenance. I specialize in debugging and troubleshooting, system design, development, testing, and performance optimization.</p>
             <p>I am skilled in organizing document reports, training materials, and requirement specifications. I actively participate in team meetings and training programs, always striving to enhance my skills and knowledge.</p>
-            <a href="./assets/resume.pdf" className="btn" target="_blank" rel="noopener noreferrer">Download My Resume</a>
+            <a href={resume} className="btn" target="_blank" rel="noopener noreferrer">Download My Resume</a>
         </div>
     );
 }
